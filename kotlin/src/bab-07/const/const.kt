2f6d1ed7ab36b4a0b1e93c5be8b75ece6e59653f
Hellenistic Philosopher <phylossophie@gmail.com>
2018-09-16 10:32:02 +0700

    Add ch 7

---
+++ kotlin/src/bab-07/const/const.kt
@@ -0,0 +1,10 @@
+const val startDay: String = "Monday"
+
+fun main(args : Array<String>) {
+
+  // what if we put const decalaration here:
+  // const val startDay: String = "Monday"
+
+  println("Hello, $startDay!")
+
+}
---
+++ kotlin/src/bab-07/variabel/variabel.kt
@@ -0,0 +1,44 @@
+const val startDay: String = "Monday"
+
+fun main(args : Array<String>) {
+
+  val immu1 = 10
+  val immu2 = "Wabi"
+  val immu3: Int = 200
+  val immu4: Int
+
+  immu4 = 250
+
+  // what if we change the value?
+  // immu4 = 100
+
+  println(immu1)
+  println(immu2)
+  println(immu3)
+  println(immu4)
+
+  var mu1 = 10
+  var mu2: String = "Angka 200"
+  var mu3: Int = 200
+  var mu4: Int
+
+  mu4 = 250
+
+  println(mu1)
+  println(mu2)
+  println(mu3)
+  println(mu4)
+
+  // what if we change the value?
+  mu4 = 300
+
+  println(mu4)
+  // what if we change the type?
+  // mu4 = "Angka 300"
+
+  //const val SUBSYSTEM_DEPRECATED: String = "This subsystem is deprecated"
+  //const val startDay: String = "Monday"
+
+  println(startDay)
+
+}
