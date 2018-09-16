8f8b71dd84d05df67bc5d50c88c7a79415db8b9c
Hellenistic Philosopher <phylossophie@gmail.com>
2018-09-16 12:31:15 +0700

    Add ch 7 - operator

---
+++ kotlin/src/bab-07/operator/operator.kt
@@ -0,0 +1,39 @@
+import java.io.File
+
+fun main(args : Array<String>) {
+
+  val a = 10.0
+  val b = 200
+  var c: Double
+
+  val name = "Wabi"
+  val company = name
+  val difName = "Wabi"
+
+  val f1 = File("operator.kt")
+  val f2 = File("operator.kt")
+
+  c = 10.5
+
+  println(a > b)
+  println(a+b)
+  println(a/b)
+
+  c += a
+  println(c)
+
+  c %= a
+  println(c)
+
+  c++
+  println(c)
+
+  println(!(a > b))
+
+  println(a==10.0)
+  println(company===name)
+  println(name=="Wabi")
+  println(company===difName)
+  println(f1===f2)
+
+}
