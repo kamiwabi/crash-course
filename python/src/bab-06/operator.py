74137eb9086c433cef9adf389f6c78bcd4ed5c84
Hellenistic Philosopher <phylossophie@gmail.com>
2018-10-25 00:13:31 +0700

    Add some source

---
+++ python/src/bab-06/operator.py
@@ -0,0 +1,52 @@
+print('Operator Aritmetika')
+print(21+22)
+print(34-14)
+print(2*3)
+print(21/2)
+print(21.00/2.00)
+print(21%2)
+print(21.00//2.00)
+print(4**3)
+print('Operator Relasional / Perbandingan')
+print(3>22)
+print(3<22)
+print(4<=4)
+print(4>=4)
+print(5==5.0)
+print(1!=1.0)
+
+print('Operator Penugasan / Assignment')
+x = 50
+print(x)
+x+=5
+print(x)
+x-=2
+print(x)
+x*=2
+print(x)
+x/=2
+print(x)
+x%=3
+print(x)
+x//=2
+print(x)
+x**=2
+print(x)
+x&=2
+print(x)
+x|=2
+print(x)
+x^=2
+print(x)
+x>>=2
+print(x)
+x<<=2
+print(x)
+
+print('Operator Logika')
+
+print('Operator Keanggotaan / Membership')
+
+print('Operator Identitas / Identity')
+
+print('Operator Bitwise')
