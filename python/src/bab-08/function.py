3c9a66e906c066ac1ff324e442f86679569aae75
Bambang Purnomosidi D. P <phylossophie@gmail.com>
2018-11-28 23:02:25 +0700

    Add contents

---
+++ python/src/bab-08/function.py
@@ -0,0 +1,8 @@
+def jumlah(arg1):
+    jml = 0
+    for a in arg1:
+        jml += 1
+    return jml
+
+jml_str = jumlah("owaladahalh")
+print("String hwarakadah mempunyai " + str(jml_str) + " karakter")
