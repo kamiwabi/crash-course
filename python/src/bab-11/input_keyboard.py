047116dd6005fd1dd32dd342c1d91dffbd6cba45
Bambang Purnomosidi D. P <phylossophie@gmail.com>
2018-11-29 22:48:04 +0700

    Add contents

---
+++ python/src/bab-11/input_keyboard.py
@@ -0,0 +1,8 @@
+# input_keyboard.py
+
+nama = input('Masukkan nama = ')
+usia = input('Umur = ')
+
+print(f'Nama = {nama}, usia = {usia}')
+print('Nama = {}, usia = {}'.format(nama, usia))
+
---
+++ python/src/bab-11/open_file_with.py
@@ -0,0 +1,6 @@
+# open_file_with.py
+
+with open('angka.txt') as f:
+    read_data = f.read()
+    print(read_data)
+    f.closed
