291a91d4d2d80135e1d61b95178d602a4d2f792e
Bambang Purnomosidi D. P <phylossophie@gmail.com>
2018-11-30 05:09:02 +0700

    Add source code

--- python/src/bab-11/input_keyboard.py
+++ python/src/bab-11/input_keyboard.py
@@ -1,8 +1,8 @@
 # input_keyboard.py
 
 nama = input('Masukkan nama = ')
-usia = input('Umur = ')
+usia = int(input('Umur = '))
 
 print(f'Nama = {nama}, usia = {usia}')
-print('Nama = {}, usia = {}'.format(nama, usia))
+print('Nama = {0:^}, usia = {1:4d}'.format(nama, usia))
 
